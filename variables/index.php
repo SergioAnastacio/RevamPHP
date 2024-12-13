62aa02aac1ec4fea929fc3f69a5155b28ee729fb
SergioAngulo <sergioangulorosales@gmail.com>
2024-12-12 22:51:08 -0600

    Features/ variables

---
+++ variables/index.php
@@ -0,0 +1,19 @@
+<?php
+//Variables never start with a number
+$var1 = 10;
+//variables is case sensitive
+$case1 = 20; //different variable
+$Case1 = 30; //different variable 
+// Concatenation
+$hello = "Hello \n";
+$world = "World \n";
+echo $hello . " " . $world; // output: Hello World
+
+//soft typing in php
+$var1 = 10;
+echo $var1."\n"; //output: 10
+$var1 = "Hello \n";
+echo $var1; //output: Hello
+
+
+?>
